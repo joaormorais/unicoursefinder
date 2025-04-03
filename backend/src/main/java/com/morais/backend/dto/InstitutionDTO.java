0416cf499556bbd49cde9880389dd1669069c250
Joao Morais <72463113+joaormorais@users.noreply.github.com>
2025-04-03 17:51:35 +0100

    Created DTO's so the info can be returned in a more controlled way

---
+++ backend/src/main/java/com/morais/backend/dto/InstitutionDTO.java
@@ -0,0 +1,16 @@
+package com.morais.backend.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record InstitutionDTO(
+        Long id,
+        String name,
+        String normalizedName,
+        String type,
+        String normalizedType,
+        String district,
+        BigDecimal latitude,
+        BigDecimal longitude,
+        List<Long> courseIds
+) {}
