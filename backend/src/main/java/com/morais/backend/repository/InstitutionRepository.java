524d5b1c19ac97ea7fe99d10b777e89e28c3e374
Joao Morais <72463113+joaormorais@users.noreply.github.com>
2025-04-03 17:54:00 +0100

    Fixed the queries in order to match the entities

--- backend/src/main/java/com/morais/backend/repository/InstitutionRepository.java
+++ backend/src/main/java/com/morais/backend/repository/InstitutionRepository.java
@@ -2,16 +2,20 @@ package com.morais.backend.repository;
 
 import com.morais.backend.entity.Institution;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface InstitutionRepository extends JpaRepository<Institution, String> {
+public interface InstitutionRepository extends JpaRepository<Institution, Long> {
 
-    List<Institution> findInstitutionsByName(String institutionName);
+    @Query("SELECT i FROM Institution i WHERE i.normalizedName LIKE %:name%")
+    List<Institution> findByNormalizedNameContaining(@Param("name") String name);
 
-    List<Institution> findInstitutionsByType(String type);
+    @Query("SELECT i FROM Institution i WHERE i.normalizedType LIKE %:type%")
+    List<Institution> findByNormalizedTypeContaining(@Param("type") String type);
 
     List<Institution> findInstitutionsByDistrict(String district);
 }
