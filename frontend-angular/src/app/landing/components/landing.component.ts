d9c6ff0b3ea4e05a6ff6eef3190e3fc66448b44b
Joao Morais <72463113+joaormorais@users.noreply.github.com>
2025-06-12 18:41:32 +0100

    Finished the landing page

--- frontend-angular/src/app/landing/components/landing.component.ts
+++ frontend-angular/src/app/landing/components/landing.component.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 import { TranslatePipe } from '@ngx-translate/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-landing',
-  imports: [CommonModule, TranslatePipe, MatButtonModule, RouterModule],
+  imports: [
+    CommonModule,
+    TranslatePipe,
+    MatButtonModule,
+    RouterModule,
+    MatIconModule,
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
