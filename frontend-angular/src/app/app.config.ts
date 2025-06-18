1f081e923a64acaa3819460dc71b5aa74bb5b1ba
Joao Morais <72463113+joaormorais@users.noreply.github.com>
2025-06-18 02:40:16 +0100

    Updated versions (19). Added PrimeNG

--- frontend-angular/src/app/app.config.ts
+++ frontend-angular/src/app/app.config.ts
@@ -1,14 +1,12 @@
-import {
-  ApplicationConfig,
-  provideZoneChangeDetection,
-  inject,
-  provideAppInitializer,
-} from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, HttpClient } from '@angular/common/http';
 import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export function httpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -25,6 +23,12 @@ export const appConfig: ApplicationConfig = {
         provide: TranslateLoader,
         useFactory: httpLoaderFactory,
         deps: [HttpClient],
+      },
+    }),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
       },
     }),
   ],
