10477348d099f8016fc5b0f6447fd8c0b7fdcaff
Joao Morais <72463113+joaormorais@users.noreply.github.com>
2025-06-25 16:22:58 +0100

    Added primeng to the landing component

--- frontend-angular/src/app/landing/components/landing.component.ts
+++ frontend-angular/src/app/landing/components/landing.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
-import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-landing',
-  imports: [TranslatePipe, MatButtonModule, RouterModule, MatIconModule],
+  imports: [TranslatePipe, RouterModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
