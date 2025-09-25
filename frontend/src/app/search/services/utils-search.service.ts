820c3537a77c2aab078a669d4a65b24b3f57f9fe
Joao Morais <72463113+joaormorais@users.noreply.github.com>
2025-09-25 01:00:26 +0100

    Utils service

---
+++ frontend/src/app/search/services/utils-search.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PaginatedReferences } from '../../shared/models/shared.model';
+import { environment } from '../../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class UtilsSearchService {
+  private http = inject(HttpClient);
+
+  // api call to get every institution ready for the dropdown
+  getDropdown(
+    page: number,
+    size: number,
+    name: string,
+    apiUrl: string
+  ): Observable<PaginatedReferences> {
+    const baseParams = new HttpParams()
+      .set('page', page)
+      .set('size', size)
+      .set('name', name);
+
+    return this.http.get<PaginatedReferences>(
+      `${environment.apiBaseUrl}/` + apiUrl + '/dropdown',
+      {
+        params: baseParams,
+      }
+    );
+  }
+}
