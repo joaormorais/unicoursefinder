1cf7ae8e43ad4ef7177c133b4f61b37914d237a3
Joao Morais <72463113+joaormorais@users.noreply.github.com>
2025-11-02 02:50:36 +0000

    Removed comments pt.1

--- backend/src/main/java/com/morais/backend/controller/CourseController.java
+++ backend/src/main/java/com/morais/backend/controller/CourseController.java
@@ -22,17 +22,6 @@ public class CourseController {
 
     private final CourseService courseService;
 
-    /**
-     * Searches for courses with optional filters for name, types and institutions.
-     * The results are paged and sorted.
-     *
-     * @param pageable         object that is going to be used to pagination and sorting
-     * @param dgesNumber       dgesNumber filter
-     * @param name             name filter
-     * @param types            type filter
-     * @param institutionUuids institution id filter
-     * @return a list of courses matching the search criteria
-     */
     @GetMapping
     public ResponseEntity<Page<CourseDto>> searchCourses(
             @PageableDefault(size = 5, sort = "normalizedName") Pageable pageable,
@@ -54,11 +43,6 @@ public class CourseController {
         return ResponseEntity.ok(courseService.getDropdown(pageable, name));
     }
 
-    /**
-     * Retrieves a list of all course types.
-     *
-     * @return a list of unique course types
-     */
     @GetMapping("/types")
     public ResponseEntity<List<String>> getTypes() {
         return ResponseEntity.ok(courseService.getTypes());
